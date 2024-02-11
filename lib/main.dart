fb8caed895a1148dc83e88fba9f2d75b0499381c
ProminenceFM <folarinaluko123@yahoo.com>
2024-02-11 15:36:34 +0100

    feat: Styling to the MakeTitle section and Video Section

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class foodApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Food App',
       theme: ThemeData(
+        fontFamily:'Poppins',
         primaryColor: const Color(0x30303030),
       ),
       home: const FoodPreparePage(),
@@ -33,7 +34,7 @@ class FoodPreparePage extends StatelessWidget {
           ),
           actions: [
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              padding: EdgeInsets.symmetric(horizontal: 15.0),
               child: IconButton(
                 onPressed: () {},
                 icon: const Icon(Icons.more_horiz),
@@ -46,14 +47,15 @@ class FoodPreparePage extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 20.0),
             child:
                 Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              MakePageTitleSection(maketitle: "How to make French Toast"),
+              MakePageTitleSection(maketitle: "How to make french toast"),
               MakePageVideo(videoimage: 'assets/images/img1.png'),
               MakePageReviewSection(
                   rating: '4,5', countreviews: '(300 reviews)'),
               MakePageAccountSection(
                   accountimage: 'assets/images/unplashimg.png',
                   accountname: 'Robert Anny',
-                  accountlocation: 'Bali, Indonesia')
+                  accountlocation: 'Bali, Indonesia'),
+              MakePageIngredientsSection(),
             ]),
           ),
         ));
@@ -71,9 +73,10 @@ class MakePageTitleSection extends StatelessWidget {
       Text(
         maketitle,
         style: const TextStyle(
-            fontWeight: FontWeight.w900,
-            fontSize: 25.0,
-            fontFamily: 'Poppins',
+            fontFamily:'Poppins',
+            height: 1.2,
+            fontWeight: FontWeight.w600,
+            fontSize: 26.0,
             color: Colors.black),
         textAlign: TextAlign.left,
       )
@@ -88,20 +91,23 @@ class MakePageVideo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Stack(alignment: Alignment.center, children: [
-      ClipRRect(
-        borderRadius: BorderRadius.circular(15.0),
-        child: Expanded(
-          child: Image.asset(
-            videoimage,
-            height: 200,
-            fit: BoxFit.cover,
+    return Container(
+      margin:  EdgeInsets.symmetric(vertical: 18.0),
+      child: Center(
+          child: Stack(alignment: Alignment.center, children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(15.0),
+          child: Expanded(
+            child: Image.asset(
+              videoimage,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
           ),
         ),
-      ),
-      Image.asset('assets/images/Playbutton.png'),
-    ]));
+        Image.asset('assets/images/Playbutton.png'),
+      ])),
+    );
   }
 }
 
@@ -115,16 +121,22 @@ class MakePageReviewSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.bottom,
       children: [
         Icon(
           Icons.star,
-          color: Color.fromARGB(255, 238, 255, 0),
+          color: Color.fromARGB(255, 255, 217, 0),
+          size: 18.0,
         ),
-        Text(
-          rating,
-          style: const TextStyle(
-            color: Colors.black,
+        Padding(
+          padding:const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Text(
+            rating,
+            style: const TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
         Text(
@@ -151,50 +163,40 @@ class MakePageAccountSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Row(
-          children: [
-            Image.asset(accountimage),
-            Column(children: [
-              Text(accountname),
-              Row(children: [
-                Image.asset('assets/images/Location.png'),
-                Text(accountlocation)
-              ])
-            ])
-          ],
-        ),
-        ElevatedButton(onPressed: () {}, child: Text('Follow'))
-      ],
-    );
-  }
-}
-
-class MakePageIngredientsSection extends StatelessWidget {
-  const MakePageIngredientsSection({super.key});
-
-  Widget build(BuildContext context) {
-    return const Column(children: [
-      Row(
+    return Container(
+      margin:  EdgeInsets.symmetric(vertical: 18.0),
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [Text('Ingredients'), Text('5 Items')],
+        children: [
+          Row(
+            children: [
+              Container(margin: EdgeInsets.only(right: 10.0),
+                child: Image.asset(
+                  accountimage),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Text(accountname),
+                Row(children: [
+                  Image.asset('assets/images/Location.png'),
+                  Text(accountlocation)
+                ])
+              ])
+            ],
+          ),
+          ElevatedButton(onPressed: () {}, 
+            child: Text('Follow'),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.red,
+              foregroundColor: Colors.white,
+              padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5), // <-- Radius
+              ),
+            ),)
+        ],
       ),
-      MakePageIngredientColumn(),
-    ]);
-  }
-}
-
-class MakePageIngredientColumn extends StatelessWidget {
-  const MakePageIngredientColumn({super.key});
-
-  Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        MakePageIngredient(ingredientimage: 'assets/images/food1.png', ingredientname: 'Bread', ingredientweight: '200g'),
-        MakePageIngredient(ingredientimage: 'assets/images/food2.png', ingredientname: 'Eggs', ingredientweight: '200g'),
-        MakePageIngredient(ingredientimage: 'assets/images/food1.png', ingredientname: 'Milk', ingredientweight: '200g')
-      ],
     );
   }
 }
@@ -211,34 +213,71 @@ class MakePageIngredient extends StatelessWidget {
   final String ingredientweight;
 
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Column(
       children: [
-        Row(
-          children: [Image.asset(ingredientimage), Text(ingredientname)],
+        Container(
+          padding: EdgeInsets.all(12),
+          margin: EdgeInsets.only(top: 15.0),
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),
+            border: Border.all(color: Colors.transparent, width: 2),
+            color: Colors.blueGrey.withAlpha(50),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(3),
+                    margin: EdgeInsets.only(right: 4.0),
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(4),
+                    border: Border.all(color: Colors.transparent, width: 2),
+                     color: Colors.white,
+                      ),
+                    child: Image.asset(ingredientimage)),
+                Text(ingredientname, 
+                  style:TextStyle(color:Colors.black,
+                   fontWeight:FontWeight.bold)
+                   )
+                   ],
+              ),
+              Text(ingredientweight,style:TextStyle(color:Colors.black))
+            ],
+          ),
         ),
-        Text(ingredientweight)
       ],
     );
   }
 }
 
-// class AppBarComponent extends StatelessWidget {
-//   const AppBarComponent({super.key});
+class MakePageIngredientColumn extends StatelessWidget {
+  const MakePageIngredientColumn({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return AppBar(
-//       leading: IconButton(
-//         onPressed: (){},
-//         icon: const Icon(Icons.arrow_back),
-//       ),
-//       actions: [
-//         IconButton(
-//           onPressed: (){},
-//           icon: const Icon(Icons.more_vert),
-//         ),
-//       ],
-//     );
-//   }
-// }
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        MakePageIngredient(ingredientimage: 'assets/images/food1.png', ingredientname: 'Bread', ingredientweight: '200g'),
+        MakePageIngredient(ingredientimage: 'assets/images/food1.png', ingredientname: 'Bread', ingredientweight: '200g'),
+        MakePageIngredient(ingredientimage: 'assets/images/food1.png', ingredientname: 'Bread', ingredientweight: '200g'),
+        MakePageIngredient(ingredientimage: 'assets/images/food2.png', ingredientname: 'Eggs', ingredientweight: '200g'),
+        MakePageIngredient(ingredientimage: 'assets/images/food1.png', ingredientname: 'Milk', ingredientweight: '200g')
+      ],
+    );
+  }
+}
+
+class MakePageIngredientsSection extends StatelessWidget {
+  const MakePageIngredientsSection({super.key});
+
+  Widget build(BuildContext context) {
+    return const Column(
+      
+      children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [Text('Ingredients', style:TextStyle(color:Colors.black)), Text('5 Items', style:TextStyle(color:Colors.black))],
+      ),
+      MakePageIngredientColumn(),
+    ]);
+  }
+}
